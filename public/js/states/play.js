a079d24628326b2a646208ae6a9b8f00f63d874f
Harry Boyes <harry.boyes@gmail.com>
2016-07-17 16:58:21 +0100

    Implement sprite movement

--- public/js/states/play.js
+++ public/js/states/play.js
@@ -1,9 +1,48 @@
 var playState = {
   create: function(){
-    this.playerSprite = game.add.graphics(400, 300);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.playerSprite.beginFill(0xFFFF00);
-    this.playerSprite.drawRect(-50, -50, 100, 100);
-    this.playerSprite.endFill();
+    this.playerSprite = initialisePlayer();
   },
+
+  update: function(){
+    updatePlayerRotation(this.playerSprite);
+    updatePlayerMovement(this.playerSprite);
+  }
 }
+
+var initialisePlayer = function(){
+  var playerSprite = game.add.graphics(400, 300);
+
+  playerSprite.beginFill(0xFFFF00);
+  playerSprite.drawRect(-50, -50, 100, 100);
+  playerSprite.endFill();
+
+  game.physics.enable(playerSprite, Phaser.Physics.ARCADE);
+
+  playerSprite.body.allowRotation = true;
+  playerSprite.rotationSpeed = 0.05;
+
+  playerSprite.forwardSpeed = 200;
+
+  return playerSprite
+}
+
+var updatePlayerRotation = function(playerSprite){
+  if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    playerSprite.rotation -= playerSprite.rotationSpeed;
+
+  if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    playerSprite.rotation += playerSprite.rotationSpeed;
+};
+
+var updatePlayerMovement = function(playerSprite){
+  if(game.input.keyboard.isDown(Phaser.Keyboard.UP))
+    game.physics.arcade.velocityFromRotation(playerSprite.rotation, playerSprite.forwardSpeed, playerSprite.body.velocity);
+  else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+    game.physics.arcade.velocityFromRotation(playerSprite.rotation, -playerSprite.forwardSpeed, playerSprite.body.velocity);
+  else {
+    playerSprite.body.velocity.x = 0;
+    playerSprite.body.velocity.y = 0;
+  }
+};
